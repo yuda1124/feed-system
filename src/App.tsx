5f895098b2f0477f5e0bad3207e167caf6088acd
yuda1124 <yuda1124@naver.com>
2021-05-13 23:12:55 +0900

    feat: react-router 설치 및 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { Main } from './pages';
 
-function App() {
-  return <div className="App" />;
-}
-
+const App = () => (
+  <Switch>
+    <Route path="/" exact component={Main} />
+  </Switch>
+);
 export default App;
