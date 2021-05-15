35ffe6b7309eaa573299718d76d5939506955517
yuda1124 <yuda1124@naver.com>
2021-05-15 21:11:25 +0900

    feat: API Request 함수 일부 작성
    - serialize 함수 작성
    - getCategories, getFeedList, getAdvertisementList 함수 작성

---
+++ src/apis/index.ts
@@ -0,0 +1,42 @@
+import endpoint from './endpoint';
+import {
+  CategoryResponse,
+  FeedListParams,
+  FeedListResponse,
+  AdvertisementListParams,
+  AdvertisementListResponse,
+} from '../types';
+
+const serializeParameter = (obj: { [key: string]: any }) => {
+  const str: string[] = [];
+  Object.keys(obj).forEach(key => {
+    if (Array.isArray(obj[key])) {
+      obj[key].forEach((val: any) => {
+        str.push(`${encodeURIComponent(key)}[]=${encodeURIComponent(val)}`);
+      });
+    } else {
+      str.push(`${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`);
+    }
+  });
+  return str.join('&');
+};
+
+export const getCategories = async () => {
+  const response: CategoryResponse = await endpoint.get('/category');
+  const {
+    data: { category },
+  } = response;
+  return category;
+};
+
+export const getFeedList = async ({ page, ord, category, limit }: FeedListParams) => {
+  const response: FeedListResponse = await endpoint.get(`/list?${serializeParameter({ page, ord, category, limit })}`);
+  const { data } = response;
+  return data;
+};
+
+export const getAdvertisementList = async ({ page, limit }: AdvertisementListParams) => {
+  const response: AdvertisementListResponse = await endpoint.get(`/ads?${serializeParameter({ page, limit })}`);
+  const { data } = response;
+  return data;
+};
