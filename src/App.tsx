a0a08ff9d24df64d914c0e780c92555c26cb9efb
yuda1124 <yuda1124@naver.com>
2021-05-16 02:14:33 +0900

    feat: Header 컴포넌트 추가, App 컴포넌트 스타일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Main } from './pages';
+import { Header } from './components';
+import './App.style.scss';
 
 const App = () => (
-  <Switch>
-    <Route path="/" exact component={Main} />
-  </Switch>
+  <div className="app">
+    <Header />
+    <Switch>
+      <Route path="/" exact component={Main} />
+    </Switch>
+  </div>
 );
 export default App;
