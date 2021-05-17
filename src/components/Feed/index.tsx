1bb3a044b1be42913f427b8be026fd09de89cc55
yuda1124 <yuda1124@naver.com>
2021-05-18 02:52:49 +0900

    refactor: FeedDetail, FeedSummary, Advertisement, Reply 타입 모두 적용할 수 있도록 Refactoring

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,32 +1,48 @@
 import React, { memo } from 'react';
-import { FeedSummary, Category } from '../../types';
+import { FEED_TYPE, TFeed } from '../../types';
 import { formatDate } from '../../utils';
 import './style.scss';
 
 type FeedProps = {
-  feed: FeedSummary;
-  categories: Category[];
+  feed: TFeed;
 };
-const areEqual = (prevProps: FeedProps, nextProps: FeedProps) => prevProps.feed.id === nextProps.feed.id;
-
-const Feed = memo(({ feed, categories }: FeedProps) => {
-  const { category_id, id, contents, title, created_at, user_id } = feed;
-  const category = categories.find(c => c.id === category_id);
+const areEqual = (prevProps: FeedProps, nextProps: FeedProps) =>
+  prevProps.feed.id === nextProps.feed.id && prevProps.feed.feedType === nextProps.feed.feedType;
+const Feed = memo(({ feed }: FeedProps) => {
+  const { id, contents, feedType, created_at, category } = feed;
+  const userInfo =
+    feed.feedType === FEED_TYPE.ADVERTISEMENT
+      ? 'sponsered'
+      : feed.feedType === FEED_TYPE.SUMMARY
+      ? feed.user_id
+      : feed.user.name;
   return (
-    <div className="wrap_feed">
-      <div className="header_feed">
-        <span className="category_name">{category?.name}</span>
-        <span className="feed_id">{id}</span>
-      </div>
+    <div className={`wrap_feed ${feedType}`}>
+      {feedType === FEED_TYPE.SUMMARY && (
+        <div className="header_feed">
+          <span className="category_name">{category?.name}</span>
+          <span className="feed_id">{id}</span>
+        </div>
+      )}
       <div className="main_feed">
         <div className="info_feed">
-          <span className="user_id">{user_id}</span>
-          <span>{formatDate(created_at)}</span>
+          <span className="user_info">{userInfo}</span>
+          {feedType === FEED_TYPE.SUMMARY && <span className="created_at">{formatDate(created_at)}</span>}
         </div>
-        <div className="content_feed">
-          <title>{title}</title>
-          <p>{contents}</p>
+        <div className="wrap_contents">
+          {feed.feedType === FEED_TYPE.ADVERTISEMENT && (
+            <img
+              src={`${process.env.REACT_APP_IMAGE_PATH as string}${feed.img}`}
+              alt={feed.title}
+              className="img_adv"
+            />
+          )}
+          <div className="content_feed">
+            <title>{feed.feedType === FEED_TYPE.REPLY ? feed.user.name : feed.title}</title>
+            <p>{contents}</p>
+          </div>
         </div>
+        {feedType === FEED_TYPE.DETAIL && <p className="created_at">{formatDate(created_at)}</p>}
       </div>
     </div>
   );
