57411583b00e6ac827821d36f5d9698f0205864f
yuda1124 <yuda1124@naver.com>
2021-05-18 05:05:21 +0900

    feat: Detail Page이고 모바일일때 뒤로가기 버튼 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useDeviceDetect } from '../../hooks';
+import { useHistory, useRouteMatch } from 'react-router-dom';
 import './style.scss';
 
-const Header = () => (
-  <header>
-    <span>[5/18] 김유영</span>
-  </header>
-);
+const Header = () => {
+  const { isMobile } = useDeviceDetect();
+  const history = useHistory();
+  const isDetailPage = useRouteMatch('/:id') !== null;
+
+  return (
+    <header>
+      {isMobile && isDetailPage && (
+        <button type="button" onClick={() => history.goBack()}>
+          🔙
+        </button>
+      )}
+      <span>[5/18] 김유영</span>
+    </header>
+  );
+};
 
 export { Header };
